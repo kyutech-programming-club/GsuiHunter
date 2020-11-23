078dacf5b34d8c8d1a658fd07c81b72b8baabd47
syu-kwsk <wataru1165kwsk@gmail.com>
2020-11-23 23:31:19 +0900

    Show questlist by gridview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:g_sui_hunter/quest_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,59 +12,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '自炊ハンター',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('自炊ハンター'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        body: ChangeNotifierProvider(
+          create: (_) => QuestModel()..getQuest(),
+          child: Consumer<QuestModel>(
+            builder: (context, model, child) {
+              final questList = model.questList.map(
+                    (quest) => Card(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(quest.name),
+                      ),
+                      Image.network(quest.pictureUrl)
+                    ],
+                  ),
+                ),
+              ).toList();
+
+              return  GridView.count(
+                mainAxisSpacing: 20.0,
+                crossAxisSpacing: 20.0,
+                crossAxisCount: 2,
+                children: questList,
+              );
+            },
+          ),
+        ),
       ),
     );
   }
