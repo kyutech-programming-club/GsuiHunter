f4d3774a9baeba1cc582988b61eb1d642df85fe6
light-ai <yoneda.yuki785@mail.kyutech.jp>
2020-11-28 19:50:33 +0900

    Add TextField

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,12 +19,62 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('自炊ハンター'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.portrait_rounded),
+            ),
+          ],
         ),
         body: Column(
-          children: <Widget>[
-            Text("User"),
-            Text("questAdd"),
-            Expanded(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    child: ButtonTheme(
+                      minWidth: 40.0,
+                      height: 20.0,
+                      child:  RaisedButton(
+                        child: Text("クエスト受注中"),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Text(
+                    "GR0",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "ハンター",
+                    style: TextStyle(
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+              TextField(
+                enabled: true,
+                maxLength: 20,
+                maxLengthEnforced: false,
+                style: TextStyle(color: Colors.black),
+                obscureText: false,
+                maxLines:1 ,
+                decoration: InputDecoration(
+                    labelText: "クエスト名",
+                    hintText: "ここにクエスト名を入力してください"),
+              ),
+              Expanded(
               child: SizedBox(
                 height: 200.0,
                 child: ChangeNotifierProvider(
@@ -38,11 +87,11 @@ class MyApp extends StatelessWidget {
                           ),
                       ).toList();
                       return GridView.count(
-                          mainAxisSpacing: 20.0,
-                          crossAxisSpacing: 20.0,
-                          crossAxisCount: 2,
-                          children: questList,
-                        );
+                        mainAxisSpacing: 20.0,
+                        crossAxisSpacing: 20.0,
+                        crossAxisCount: 2,
+                        children: questList,
+                      );
                     },
                   ),
                 ),
