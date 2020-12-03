b19ad8d64364ca63fca9f73efe19d935a1de252c
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-04 02:50:27 +0900

    Add hunter model to get hunter data

---
+++ lib/models/hunter_model.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:g_sui_hunter/models/hunter.dart';
+
+class HunterModel extends ChangeNotifier {
+  HunterModel(this.currentUser);
+
+  User currentUser;
+  Hunter hunter;
+
+  Future getHunter() async {
+    final hunterRef = FirebaseFirestore.instance.collection('hunters').doc(currentUser.uid);
+    final hunterSnapShot = await hunterRef.get();
+
+    if (hunterSnapShot.exists) {
+      this.hunter = Hunter(hunterSnapShot);
+      print("Welcome back");
+    } else {
+      hunterRef.set({
+        'name': currentUser.displayName,
+        'rank': 1,
+        'exp': 0,
+        'currentQuest': null,
+      })
+          .then((value) async {
+        this.hunter = Hunter(await hunterRef.get());
+        print("New Hunter Added");
+      })
+          .catchError((error) => print("Failed to add hunter: $error"));
+    }
+    notifyListeners();
+  }
+}
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:g_sui_hunter/models/hunter_model.dart';
 import 'package:g_sui_hunter/models/user_auth_model.dart';
 import 'package:provider/provider.dart';
 
@@ -7,22 +8,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = context.select<UserAuthModel, User>((value) => value.user);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HOME'),
-        actions: [
-          CircleAvatar(
-            backgroundImage: NetworkImage(user.photoURL),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => HunterModel(user),
+        ),
+      ],
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('HOME'),
+            actions: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(user.photoURL),
+              ),
+              IconButton(
+                icon: Icon(Icons.logout),
+                onPressed: () async {
+                  await context.read<UserAuthModel>().signOut();
+                },
+              ),
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              await context.read<UserAuthModel>().signOut();
-            },
+          body: Center(
+            child: RaisedButton(
+              child: Text('create hunter'),
+              onPressed: () => context.read<HunterModel>().getHunter(),
+            ),
           ),
-        ],
-      ),
-      body: Container(),
+        );
+      },
     );
   }
 }
