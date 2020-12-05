e477f8e5f0e80a4fc50a2e50861674b0ce4ff5de
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-06 01:59:42 +0900

    Wait for fetching tag data is completeed

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,9 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:g_sui_hunter/models/hunter.dart';
 import 'package:g_sui_hunter/models/hunter_model.dart';
-import 'package:g_sui_hunter/models/quest_model.dart';
-import 'package:g_sui_hunter/models/user_auth_model.dart';
+import 'package:g_sui_hunter/models/tag.dart';
+import 'package:g_sui_hunter/models/tag_model.dart';
 import 'package:g_sui_hunter/views/quest_list_page.dart';
 import 'package:provider/provider.dart';
 
@@ -11,7 +10,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final hunter = context.select<HunterModel, Hunter>((value) => value.hunter);
-    if (hunter == null) {
+    final tagList = context.select<TagModel, List<Tag>>((value) => value.tagList);
+    if (hunter == null || tagList == []) {
       return Scaffold(
         appBar: AppBar(),
         body: Center(
