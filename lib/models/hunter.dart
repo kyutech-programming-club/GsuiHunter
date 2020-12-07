76eafa462b632842d43f46130a9f86771ce0d5c3
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-08 01:02:48 +0900

    Add id field to get document reference

--- lib/models/hunter.dart
+++ lib/models/hunter.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Hunter{
   Hunter(DocumentSnapshot doc){
+    this.id = doc.id;
     this.name = doc.data()['name'];
     this.rank = doc.data()['rank'];
     this.exp = doc.data()['exp'];
@@ -9,10 +10,11 @@ class Hunter{
     this.quests = doc.data()['quests'];
     this.skills = doc.data()['skills'];
   }
+  String id;
   String name;
   int rank;
   int exp;
   DocumentReference currentQuest;
-  List<DocumentReference> quests;
+  List<dynamic> quests;
   Map<String, int> skills;
 }
