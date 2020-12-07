bfe20b50268bf968be02d20bed91f780142d76ae
wanwan0622 <minnie20000622@yahoo.co.jp>
2020-12-07 23:25:56 +0900

    Update cached image

--- lib/views/widgets/quest_card.dart
+++ lib/views/widgets/quest_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:g_sui_hunter/models/quest.dart';
 import 'package:g_sui_hunter/views/quest_detail_page.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class QuestCard extends StatelessWidget {
   const QuestCard({
@@ -28,7 +29,11 @@ class QuestCard extends StatelessWidget {
               title: Text(data.title),
             ),
             Expanded(
-              child: Image.network(data.imageUrl),
+              child: CachedNetworkImage(
+                placeholder: (context, url) => CircularProgressIndicator(),
+                imageUrl: data.imageUrl,
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
             ),
           ],
         ),
