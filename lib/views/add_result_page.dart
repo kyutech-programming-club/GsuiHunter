df15f77af55cbb2b1e5d79a6834fb9b0006f9268
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-09 23:10:35 +0900

    Wrap add-result by add-result-model provider

--- lib/views/add_result_page.dart
+++ lib/views/add_result_page.dart
@@ -1,53 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:g_sui_hunter/models/add_result_model.dart';
+import 'package:g_sui_hunter/models/hunter_model.dart';
 import 'package:g_sui_hunter/views/widgets/clear_comment_form.dart';
 import 'package:g_sui_hunter/views/widgets/clear_quest_button.dart';
 import 'package:g_sui_hunter/views/widgets/clear_quest_image.dart';
 import 'package:g_sui_hunter/views/widgets/clear_quest_info.dart';
 import 'package:g_sui_hunter/views/widgets/clear_time_form.dart';
+import 'package:provider/provider.dart';
 
 class AddResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('クエスト結果'),
-      ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height -
-              AppBar().preferredSize.height -
-              MediaQuery.of(context).padding.top,
-          child: Column(
-            children: [
-              Expanded(
-                flex: 4,
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: ClearQuestInfo(),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: ClearQuestImage(),
-                    ),
-                  ],
+    return ChangeNotifierProvider<AddResultModel>(
+      create: (_) {
+        final hunter = context.read<HunterModel>().hunter;
+        return AddResultModel()..createState(hunter);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('クエスト結果'),
+        ),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height -
+                AppBar().preferredSize.height -
+                MediaQuery.of(context).padding.top,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: ClearQuestInfo(),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: ClearQuestImage(),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 2,
-                child: ClearTimeForm(),
-              ),
-              Expanded(
-                flex: 3,
-                child: ClearCommentForm(),
-              ),
-              Expanded(
-                flex: 1,
-                child: ClearQuestButton(),
-              ),
-            ],
+                Expanded(
+                  flex: 2,
+                  child: ClearTimeForm(),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: ClearCommentForm(),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ClearQuestButton(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
