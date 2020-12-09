b48acae14aac5bf87a03876f94b47387325afea8
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-09 16:10:30 +0900

    Separate add-result by container and expanded

--- lib/views/add_result_page.dart
+++ lib/views/add_result_page.dart
@@ -3,8 +3,38 @@ import 'package:flutter/material.dart';
 class AddResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('クエスト結果'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.orange,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.green,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
