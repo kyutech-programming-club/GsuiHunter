54fdc8b2738ee6550df081f94f8db59a0507ce41
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-09 16:35:20 +0900

    Add comment text field

--- lib/views/add_result_page.dart
+++ lib/views/add_result_page.dart
@@ -23,8 +23,22 @@ class AddResultPage extends StatelessWidget {
           ),
           Expanded(
             flex: 3,
-            child: Container(
-              color: Colors.orange,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('コメント'),
+                  TextField(
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: 'クリアのコツや感想を教えてください',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Expanded(
