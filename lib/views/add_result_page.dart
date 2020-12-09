d6d5a7c8c3167590755effad9e4b9029af9756f1
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-09 20:56:30 +0900

    Add shadow quest-image add-result

--- lib/views/add_result_page.dart
+++ lib/views/add_result_page.dart
@@ -59,8 +59,21 @@ class AddResultPage extends StatelessWidget {
                     Expanded(
                       flex: 1,
                       child: Container(
+                        margin: EdgeInsets.all(8),
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              spreadRadius: 1.0,
+                              blurRadius: 10.0,
+                              offset: Offset(10, 10),
+                            ),
+                          ],
+                        ),
                         alignment: Alignment.center,
-                        color: Colors.orange,
                         child: FittedBox(
                           fit: BoxFit.contain,
                           child: CachedNetworkImage(
