b0344302071189b77a8ae8189661759f60ba3de9
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-09 19:49:45 +0900

    Fix textfield overflow bug

--- lib/views/add_result_page.dart
+++ lib/views/add_result_page.dart
@@ -10,135 +10,142 @@ class AddResultPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('クエスト結果'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 4,
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: LayoutBuilder(
-                    builder: (BuildContext context, BoxConstraints constraints) {
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text(
-                            'Rank0',
-                            style: TextStyle(
-                              fontSize: constraints.maxHeight * 0.1,
-                              fontWeight: FontWeight.bold,
-                            ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height -
+              AppBar().preferredSize.height -
+              MediaQuery.of(context).padding.top,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 4,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: LayoutBuilder(
+                        builder: (BuildContext context, BoxConstraints constraints) {
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                'Rank0',
+                                style: TextStyle(
+                                  fontSize: constraints.maxHeight * 0.1,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              AutoSizeText(
+                                'デミグラスハンバーグ',
+                                style: TextStyle(
+                                  fontSize: constraints.maxHeight * 0.2,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                maxLines: 2,
+                                textAlign: TextAlign.center,
+                              ),
+                              Text(
+                                'をクリア！',
+                                style: TextStyle(
+                                  fontSize: constraints.maxHeight * 0.1,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        alignment: Alignment.center,
+                        color: Colors.orange,
+                        child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: CachedNetworkImage(
+                            placeholder: (context, url) => CircularProgressIndicator(),
+                            imageUrl: '',
+                            errorWidget: (context, url, error) => Icon(Icons.error),
                           ),
-                          AutoSizeText(
-                            'デミグラスハンバーグ',
-                            style: TextStyle(
-                              fontSize: constraints.maxHeight * 0.2,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            maxLines: 2,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child:  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'クリアタイム',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Flexible(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                          child: TextField(
                             textAlign: TextAlign.center,
                           ),
-                          Text(
-                            'をクリア！',
-                            style: TextStyle(
-                              fontSize: constraints.maxHeight * 0.1,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      );
-                    },
+                        ),
+                      ),
+                      Text(
+                        '分',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.orange,
-                    child: FittedBox(
-                      fit: BoxFit.contain,
-                      child: CachedNetworkImage(
-                        placeholder: (context, url) => CircularProgressIndicator(),
-                        imageUrl: '',
-                        errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('コメント'),
+                      TextField(
+                        keyboardType: TextInputType.multiline,
+                        decoration: InputDecoration(
+                          hintText: 'クリアのコツや感想を教えてください',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: FlatButton(
+                    color: Colors.green,
+                    child: Text(
+                      'クエストクリア',
+                      style: TextStyle(
+                        color: Colors.white,
                       ),
                     ),
+                    onPressed: () => print('クエスト完了の処理'), // TODO: クエスト完了の処理
                   ),
                 ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child:  Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'クリアタイム',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Flexible(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: TextField(
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    '分',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
               ),
-            ),
+            ],
           ),
-          Expanded(
-            flex: 3,
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('コメント'),
-                  TextField(
-                    keyboardType: TextInputType.multiline,
-                    decoration: InputDecoration(
-                      hintText: 'クリアのコツや感想を教えてください',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.center,
-              child: FlatButton(
-                color: Colors.green,
-                child: Text(
-                  'クエストクリア',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                onPressed: () => print('クエスト完了の処理'), // TODO: クエスト完了の処理
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
