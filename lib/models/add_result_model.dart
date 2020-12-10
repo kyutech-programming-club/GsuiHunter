053dcf81b42f1911e690d9071721aaea7c9eb418
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-10 14:17:39 +0900

    Update hunter's rank

--- lib/models/add_result_model.dart
+++ lib/models/add_result_model.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:g_sui_hunter/models/hunter.dart';
 import 'package:g_sui_hunter/models/quest.dart';
+import 'package:g_sui_hunter/constants.dart';
 
 class AddResultModel extends ChangeNotifier {
   Hunter hunter;
@@ -14,10 +15,13 @@ class AddResultModel extends ChangeNotifier {
     final currentQuestRef = hunterData.data()['currentQuest'];
     final currentQuestData = await currentQuestRef.get();
 
+    final hunterRank = await _calcRank(hunterData, currentQuestData);
+
     hunterRef.update({
       'currentQuest': null,
       'quests': FieldValue.arrayUnion([currentQuestRef]),
-      'exp': FieldValue.increment(currentQuestData.data()['rank']*10)
+      'rank' : hunterRank,
+      'exp': FieldValue.increment(currentQuestData.data()['rank']*10),
     });
     hunter.currentQuest = null;
     notifyListeners();
@@ -30,6 +34,23 @@ class AddResultModel extends ChangeNotifier {
     this.currentQuest = Quest(questSnapshot);
 
     notifyListeners();
+  }
+
+  Future _calcRank(DocumentSnapshot hunterData, DocumentSnapshot currentQuestData) async {
+    final int preRank = await hunterData.data()['rank'];
+
+    if (preRank == 5) {
+      return preRank;
+    }
+
+    final int rankUpExp = rankUpExpRule[preRank];
+    final int preExp = await hunterData.data()['exp'];
+
+    if (rankUpExp <= preExp + preRank*10) {
+      return preRank + 1;
+    } else {
+      return preRank;
+    }
   }
 
 }
