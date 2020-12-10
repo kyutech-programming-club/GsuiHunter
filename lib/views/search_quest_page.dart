ba591b01f4ede889000921fbbe9464dffd52589d
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-11 01:06:45 +0900

    Provide search quest status

--- lib/views/search_quest_page.dart
+++ lib/views/search_quest_page.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:g_sui_hunter/models/search_quest_model.dart';
 import 'package:g_sui_hunter/views/widgets/current_quest_status.dart';
 import 'package:g_sui_hunter/views/widgets/seached_quest_list.dart';
+import 'package:provider/provider.dart';
 
 class SearchQuestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 1,
-          child: CurrentQuestStatus(),
-        ),
-        Expanded(
-          flex: 9,
-          child: SearchedQuestList(),
-        ),
-      ],
+    return ChangeNotifierProvider<SearchQuestModel>(
+      create: (context) => SearchQuestModel()..searchQuest('フライパン'),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: CurrentQuestStatus(),
+          ),
+          Expanded(
+            flex: 9,
+            child: SearchedQuestList(),
+          ),
+        ],
+      ),
     );
   }
 }
