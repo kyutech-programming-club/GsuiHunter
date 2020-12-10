b1afcb9423cb4b935ab74d56a7b29b5ecfc48dac
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-10 11:30:22 +0900

    Add hunter-profile and result-list pages

---
+++ lib/views/HunterProfilePage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HunterProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('This is hunter profile page.'),
+    );
+  }
+}
