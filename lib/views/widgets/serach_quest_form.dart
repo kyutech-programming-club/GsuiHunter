b354d7f59ccde3c6313ea6826fa7bd67d092e378
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-11 01:53:00 +0900

    Add drop down

--- lib/views/widgets/serach_quest_form.dart
+++ lib/views/widgets/serach_quest_form.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:g_sui_hunter/models/search_quest_model.dart';
+import 'package:g_sui_hunter/models/tag.dart';
+import 'package:g_sui_hunter/models/tag_model.dart';
+import 'package:provider/provider.dart';
 
 class SearchQuestForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    final tagList = context.select<TagModel, List<Tag>>((model) => model.tagList);
+    return DropdownButton(
+      value: context.select<SearchQuestModel, String>((model) => model.targetTag),
+      icon: Icon(Icons.search),
+      items: tagList.map((tag){
+        return DropdownMenuItem(
+          value: tag.name,
+          child: Text(tag.name),
+        );
+      }).toList(),
+      onChanged: (tag) => context.read<SearchQuestModel>().changeTargetTag(tag),
     );
   }
 }
