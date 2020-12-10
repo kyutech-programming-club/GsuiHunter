7177d01a7afb9e3d43abd6d12f2a40f94a46db10
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-10 10:47:27 +0900

    Move quest-list app-bar to homepage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,6 +3,7 @@ import 'package:g_sui_hunter/models/hunter.dart';
 import 'package:g_sui_hunter/models/hunter_model.dart';
 import 'package:g_sui_hunter/models/tag.dart';
 import 'package:g_sui_hunter/models/tag_model.dart';
+import 'package:g_sui_hunter/models/user_auth_model.dart';
 import 'package:g_sui_hunter/views/quest_list_page.dart';
 import 'package:provider/provider.dart';
 
@@ -19,7 +20,24 @@ class HomePage extends StatelessWidget {
         ),
       );
     } else {
-      return QuestListPage();
+      return Scaffold(appBar: AppBar(
+        title: Text('自炊ハンター'),
+        actions: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              context.select<UserAuthModel, String>((value) => value.user.photoURL),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              await context.read<UserAuthModel>().signOut();
+            },
+          ),
+        ],
+      ),
+        body: QuestListPage(),
+      );
     }
   }
 }
--- lib/views/quest_list_page.dart
+++ lib/views/quest_list_page.dart
@@ -1,9 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:g_sui_hunter/models/hunter_model.dart';
 import 'package:g_sui_hunter/models/quest.dart';
-import 'package:g_sui_hunter/models/user_auth_model.dart';
 import 'package:provider/provider.dart';
 import 'package:g_sui_hunter/models/quest_model.dart';
 import 'package:g_sui_hunter/views/widgets/quest_card.dart';
@@ -11,127 +9,110 @@ import 'package:g_sui_hunter/views/widgets/quest_card.dart';
 class QuestListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final user = context.select<UserAuthModel, User>((value) => value.user);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('自炊ハンター'),
-        actions: [
-          CircleAvatar(
-            backgroundImage: NetworkImage(user.photoURL),
-          ),
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              await context.read<UserAuthModel>().signOut();
-            },
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    child: ButtonTheme(
-                      minWidth: 40.0,
-                      height: 20.0,
-                      child:  RaisedButton(
-                        child: Selector<HunterModel, DocumentReference>(
-                          selector: (context, model) => model.hunter.currentQuest,
-                          builder: (context, currentQuest, child) {
-                            if (currentQuest == null) {
-                              return Text("クエスト未選択");
-                            } else {
-                              return Text("クエスト中");
-                            }
-                          },
-                        ),
+    return Column(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  child: ButtonTheme(
+                    minWidth: 40.0,
+                    height: 20.0,
+                    child:  RaisedButton(
+                      child: Selector<HunterModel, DocumentReference>(
+                        selector: (context, model) => model.hunter.currentQuest,
+                        builder: (context, currentQuest, child) {
+                          if (currentQuest == null) {
+                            return Text("クエスト未選択");
+                          } else {
+                            return Text("クエスト中");
+                          }
+                        },
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text(
-                    "GR0",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    "ハンター",
-                    style: TextStyle(
-                      fontSize: 40,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "クエスト追加→",
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text(
+                  "GR0",
                   style: TextStyle(
                     fontSize: 20,
                   ),
                 ),
-                Container(
-                  child: SizedBox(
-                    width: 40,
-                    height: 40,
-                    child: IconButton(
-                      icon: Icon(Icons.control_point_rounded),
-                      onPressed: () {
-                        Navigator.pushNamed(context,'/add_quest');
-                      },
-                    ),
+              ),
+              Container(
+                child: Text(
+                  "ハンター",
+                  style: TextStyle(
+                    fontSize: 40,
                   ),
                 ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 7,
-            child: RefreshIndicator(
-              onRefresh: () async => context.read<QuestModel>().fetchQuest(),
-              child: Selector<QuestModel, List<Quest>>(
-                builder: (context, model, child) {
-                  final questList = model.map((quest) =>
-                      QuestCard(
-                        data: quest,
-                      ),
-                  ).toList();
-                  return GridView.count(
-                    mainAxisSpacing: 20.0,
-                    crossAxisSpacing: 20.0,
-                    crossAxisCount: 2,
-                    children: questList,
-                  );
-                },
-                selector: (context, model) => model.questList,
               ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "クエスト追加→",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              Container(
+                child: SizedBox(
+                  width: 40,
+                  height: 40,
+                  child: IconButton(
+                    icon: Icon(Icons.control_point_rounded),
+                    onPressed: () {
+                      Navigator.pushNamed(context,'/add_quest');
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 7,
+          child: RefreshIndicator(
+            onRefresh: () async => context.read<QuestModel>().fetchQuest(),
+            child: Selector<QuestModel, List<Quest>>(
+              builder: (context, model, child) {
+                final questList = model.map((quest) =>
+                    QuestCard(
+                      data: quest,
+                    ),
+                ).toList();
+                return GridView.count(
+                  mainAxisSpacing: 20.0,
+                  crossAxisSpacing: 20.0,
+                  crossAxisCount: 2,
+                  children: questList,
+                );
+              },
+              selector: (context, model) => model.questList,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
