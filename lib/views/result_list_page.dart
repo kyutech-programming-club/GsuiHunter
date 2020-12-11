d0f91ce63fb5c2a6db3f30778de423e64083bf5b
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-12 05:46:09 +0900

    Add loading when transition to result-list

--- lib/views/result_list_page.dart
+++ lib/views/result_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:g_sui_hunter/models/hunter.dart';
 import 'package:g_sui_hunter/models/result.dart';
 import 'package:g_sui_hunter/models/result_model.dart';
 import 'package:g_sui_hunter/views/widgets/current_quest_status.dart';
@@ -6,34 +7,52 @@ import 'package:g_sui_hunter/views/widgets/result_card.dart';
 import 'package:provider/provider.dart';
 
 class ResultListPage extends StatelessWidget {
+  const ResultListPage({
+    Key key,
+    this.hunter
+  }) : super(key: key);
+
+  final Hunter hunter;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 1,
-          child: CurrentQuestStatus(),
-        ),
-        Expanded(
-          flex: 9,
-          child: RefreshIndicator(
-            onRefresh: () async => context.read<ResultModel>().fetchResult(),
-            child: Selector<ResultModel, List<Result>>(
-              selector: (context, model) => model.resultList,
-              builder: (context, model, child) {
-                final resultList = model.map((result) =>
-                    ResultCard(
-                      result: result,
-                    ),
-                ).toList();
-                return ListView(
-                  children: resultList,
-                );
-              },
+    return ChangeNotifierProvider<ResultModel>(
+      create: (context) => ResultModel()..fetchResult(hunter),
+      builder: (context, child) {
+        return Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: CurrentQuestStatus(),
             ),
-          ),
-        ),
-      ],
+            Expanded(
+              flex: 9,
+              child: Selector<ResultModel, List<Result>>(
+                selector: (context, model) => model.resultList,
+                builder: (context, model, child) {
+                  if (model.isEmpty) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    final resultList = model.map((result) =>
+                        ResultCard(
+                          result: result,
+                        ),
+                    ).toList();
+                    return RefreshIndicator(
+                      onRefresh: () async => context.read<ResultModel>().fetchResult(hunter),
+                      child: ListView(
+                        children: resultList,
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
