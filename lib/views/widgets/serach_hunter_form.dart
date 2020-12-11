b2fc777eceb70033ecfd5b2830aa12b43fad10d7
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-12 01:28:15 +0900

    Minimum search feature

--- lib/views/widgets/serach_hunter_form.dart
+++ lib/views/widgets/serach_hunter_form.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:g_sui_hunter/models/search_hunter_model.dart';
-import 'package:g_sui_hunter/models/tag.dart';
-import 'package:g_sui_hunter/models/tag_model.dart';
 import 'package:provider/provider.dart';
 
 class SearchHunterForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final tagList = context.select<TagModel, List<Tag>>((model) => model.tagList);
-    return DropdownButton(
-      value: context.select<SearchHunterModel, String>((model) => model.targetTag),
-      icon: Icon(Icons.search),
-      items: tagList.map((tag){
-        return DropdownMenuItem(
-          value: tag.name,
-          child: Text(tag.name),
-        );
-      }).toList(),
-      onChanged: (tag) {
-        context.read<SearchHunterModel>().changeTargetTag(tag);
-        context.read<SearchHunterModel>().searchQuest();
-      },
+    return Row(
+      children: [
+        Flexible(
+          child: TextField(
+            onChanged: (name) {
+              context.read<SearchHunterModel>().changeTargetHunterName(name);
+            },
+          ),
+        ),
+        FlatButton(
+          onPressed: () {
+            context.read<SearchHunterModel>().searchHunter();
+          },
+          child: Text('検索'),
+        )
+      ],
     );
   }
 }
