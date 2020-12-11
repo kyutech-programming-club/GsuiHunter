4699328baff8f5797b92de0610cb075a47eca4b0
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-11 15:50:08 +0900

    Show only result comment

--- lib/views/result_list_page.dart
+++ lib/views/result_list_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:g_sui_hunter/models/result.dart';
+import 'package:g_sui_hunter/models/result_model.dart';
+import 'package:g_sui_hunter/views/widgets/current_quest_status.dart';
+import 'package:provider/provider.dart';
 
 class ResultListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('This is result list page.'),
+    return Column(
+      children: [
+        Expanded(
+          flex: 1,
+          child: CurrentQuestStatus(),
+        ),
+        Expanded(
+          flex: 9,
+          child: RefreshIndicator(
+            onRefresh: () async => context.read<ResultModel>().fetchResult(),
+            child: Selector<ResultModel, List<Result>>(
+              selector: (context, model) => model.resultList,
+              builder: (context, model, child) {
+                final resultList = model.map((result) =>
+                    Text(
+                      result.comment,
+                    ),
+                ).toList();
+                return GridView.count(
+                  mainAxisSpacing: 20.0,
+                  crossAxisSpacing: 20.0,
+                  crossAxisCount: 1,
+                  children: resultList,
+                );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
