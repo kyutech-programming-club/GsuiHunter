5c62d2e65b13bfaef88de38ae16bc7bddc12a2af
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-11 20:50:06 +0900

    Transition to quest-detail from result-image or show dialog

--- lib/views/widgets/result_card_image.dart
+++ lib/views/widgets/result_card_image.dart
@@ -1,6 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:g_sui_hunter/models/hunter_model.dart';
 import 'package:g_sui_hunter/models/result.dart';
+import 'package:g_sui_hunter/views/quest_detail_page.dart';
+import 'package:provider/provider.dart';
 
 class ResultCardImage extends StatelessWidget {
   const ResultCardImage({
@@ -12,12 +16,41 @@ class ResultCardImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CachedNetworkImage(
-        placeholder: (context, url) => CircularProgressIndicator(),
-        imageUrl: result.quest.imageUrl,
-        errorWidget: (context, url, error) => Icon(Icons.error),
+    final hunterRank = context.select<HunterModel, int>((model) => model.hunter.rank);
+    return GestureDetector(
+      child: Center(
+        child: CachedNetworkImage(
+          placeholder: (context, url) => CircularProgressIndicator(),
+          imageUrl: result.quest.imageUrl,
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        ),
       ),
+      onTap: () {
+        if (hunterRank >= result.quest.rank) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => QuestDetailPage(data: result.quest),
+            ),
+          );
+        } else {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return CupertinoAlertDialog(
+                title: Text('挑戦できません'),
+                content: Text('これはRank${result.quest.rank}のクエストです'),
+                actions: [
+                  CupertinoDialogAction(
+                    child: Text('OK'),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
