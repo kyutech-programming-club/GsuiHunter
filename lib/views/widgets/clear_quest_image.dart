357144ced26961857cff00cbaeee95c35b9a091d
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-12 06:11:36 +0900

    Add camera button

--- lib/views/widgets/clear_quest_image.dart
+++ lib/views/widgets/clear_quest_image.dart
@@ -6,35 +6,47 @@ import 'package:provider/provider.dart';
 class ClearQuestImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(8),
-      padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.orange,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black26,
-            spreadRadius: 1.0,
-            blurRadius: 10.0,
-            offset: Offset(10, 10),
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.all(8),
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                spreadRadius: 1.0,
+                blurRadius: 10.0,
+                offset: Offset(10, 10),
+              ),
+            ],
           ),
-        ],
-      ),
-      alignment: Alignment.center,
-      child: FittedBox(
-        fit: BoxFit.contain,
-        child: Selector<AddResultModel, String>(
-          selector: (context, model) => model.currentQuest.imageUrl,
-          builder: (context, imageUrl, child) {
-            return CachedNetworkImage(
-              placeholder: (context, url) => CircularProgressIndicator(),
-              imageUrl: imageUrl,
-              errorWidget: (context, url, error) => Icon(Icons.error),
-            );
-          },
+          alignment: Alignment.center,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Selector<AddResultModel, String>(
+              selector: (context, model) => model.currentQuest.imageUrl,
+              builder: (context, imageUrl, child) {
+                return CachedNetworkImage(
+                  placeholder: (context, url) => CircularProgressIndicator(),
+                  imageUrl: imageUrl,
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                );
+              },
+            ),
+          ),
         ),
-      ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: IconButton(
+            icon: Icon(Icons.camera_alt),
+            iconSize: 50,
+            onPressed: () => print('カメラ起動'),
+          ),
+        ),
+      ],
     );
   }
 }
