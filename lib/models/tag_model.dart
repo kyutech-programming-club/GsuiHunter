6fc4b58fe329c282c04b1733f1ba971dfb538123
syu-kwsk <wataru1165kwsk@gmail.com>
2020-12-11 12:52:08 +0900

    Change funtion type to use await keyword

--- lib/models/tag_model.dart
+++ lib/models/tag_model.dart
@@ -5,7 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class TagModel extends ChangeNotifier {
   List<Tag> tagList = [];
 
-  void fetchTag() async {
+  Future fetchTag() async {
     final QuerySnapshot tagSnapshots = await FirebaseFirestore.instance.collection('tags').get();
     final tagList = tagSnapshots.docs.map((doc) => Tag(doc)).toList();
     this.tagList = tagList;
